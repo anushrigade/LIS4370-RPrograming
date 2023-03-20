4c30e5e6065faaba18ebbffdbb741eb98bb2eb20
anushrigade <122398215+anushrigade@users.noreply.github.com>
2023-03-20 16:51:44 -0400

    Add files via upload

---
+++ descriptionfile.R
@@ -0,0 +1,22 @@
+install.packages("roxygen2")
+library(roxygen2)
+
+install.packages("devtools")
+library(devtools)
+
+#'
+#' agade-LIS4370
+#' 
+#' Visualizing multivariate datasets with scatterplots
+#' 
+#' 0.0.0.9000
+#' 
+#' "Anushri Gade <anushrigade@usf.edu>"
+#' 
+#' Functions used to easily make customized multivariate scatterplots. Includes settings for point colors, sizes, and shading. 
+#' 
+#' R (>= 3.1.2)
+#' 
+#' CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+#' 
+#' true
